90d2f0d5da5cc4edef23bc9e05847aec3f1a0bf5
oubes <omargamal.bs99@gmail.com>
2026-04-03 20:25:42 +0200

    Add PDF document loader and update usage example for loading PDFs

--- src/modules/ingestion/usage_ex.py
+++ src/modules/ingestion/usage_ex.py
@@ -1,15 +1,16 @@
-from modules.ingestion.loader.document_loader import PDFDocumentLoader
-from modules.ingestion.chunker.chunker import Chunker
+from src.modules.ingestion.loader.pdf_loader import PDFDocumentLoader
+from src.modules.ingestion.chunker.chunker import Chunker
 
 loader = PDFDocumentLoader()
 chunker = Chunker()
 
-docs = loader.load_pdf("Ai System Design Competition.pdf")
+doc_name = "Millennial_Playbook_Full.pdf"
+docs = loader.load_pdf(doc_name)
 
 chunks = chunker.chunk_documents(
     documents=docs,
-    doc_name="ai_system_design"
+    doc_name=doc_name
 )
 
-print(chunks[0], "\n\n")
-print(chunks[1])
+for i in range(50):
+    print(chunks[i], end=f"\n\n{'-' * 100}\n\n")
