378dceb547b9c55d34b63ced0b8d5602cc9df444
oubes <omargamal.bs99@gmail.com>
2026-04-11 02:42:48 +0200

    feat: Add lru_cache decorator to get_settings for improved performance

--- src/bootstrap/dependencies/settings.py
+++ src/bootstrap/dependencies/settings.py
@@ -1,5 +1,6 @@
 # ---- Standard Library ----
 import logging
+from functools import lru_cache
 
 # ---- Core Config ----
 from src.core.config.loader import load_settings
@@ -9,6 +10,7 @@ logger = logging.getLogger(__name__)
 
 
 # ---- Settings Provider ----
+@lru_cache()
 def get_settings() -> AppSettings:
     logger.info("Loading application settings")
     return load_settings()
