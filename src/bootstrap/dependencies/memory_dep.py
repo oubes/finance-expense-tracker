8fb8964ec398ded6b3da12757b1298fdb6bca47f
oubes <omargamal.bs99@gmail.com>
2026-04-13 14:14:13 +0200

    refactor: Clean up memory dependency imports and remove unused services

--- src/bootstrap/dependencies/memory_dep.py
+++ src/bootstrap/dependencies/memory_dep.py
@@ -1,6 +1,5 @@
 # ---- Standard Library ----
 import logging
-from functools import lru_cache
 
 # ---- FastAPI ----
 from fastapi import Depends
@@ -11,28 +10,21 @@ from src.bootstrap.dependencies.settings_dep import get_settings
 
 # ---- Infrastructure ----
 from src.bootstrap.dependencies.vector_db_dep import get_db_client
+from src.bootstrap.dependencies.prompting_dep import get_safe_generator
+from src.bootstrap.dependencies.embeddings_dep import get_embedding
 
 # ---- Operations ----
-from src.services.memory.working.operations.stm_buffer_ops import STMBufferOps
-from src.services.memory.working.operations.session_state_ops import SessionStateOps
-from src.services.memory.working.operations.working_memory_ops import WorkingMemoryOps
-
 from src.services.memory.analytical.operations.analytics_ops import AnalyticsOps
 from src.services.memory.analytical.operations.transactions_ops import TransactionsOps
 
-from src.services.memory.conversational.operations.conversation_ops import ConversationOps
+from src.services.memory.semantic.operations.semantic_memory_ops import SemanticMemoryOps
 
-from src.services.memory.semantic.operations.tags_ops import TagsOps
 from src.services.memory.semantic.operations.vector_ops import VectorOps
 from src.services.memory.semantic.operations.user_facts_ops import UserFactsOps
 
 # ---- Queries ----
-from src.services.memory.working.queries import (
-    stm_buffer_queries,
-    session_queries,
-    working_queries,
-)
 
+from src.services.memory.semantic.queries import semantic_memory_queries
 from src.services.memory.analytical.queries import (
     analytics_queries,
     transactions_queries,
@@ -41,97 +33,70 @@ from src.services.memory.analytical.queries import (
 from src.services.memory.conversational.queries import conversation_queries
 
 from src.services.memory.semantic.queries import (
-    tags_queries,
     vector_queries,
     user_facts_queries,
 )
 
+# ---- Pipelines ----
+from src.pipelines.v1.conv_memory_pipeline import MemorySystem
+from src.pipelines.v1.memory_pipeline import MemoryPipeline
+
 # ---- Logger ----
 logger = logging.getLogger(__name__)
 
+# ---- DB CLIENT ----
+def get_db_client_dep(db=Depends(get_db_client)):
+    return db
 
-# ---- DB Client Cache ----
-@lru_cache()
-def get_cached_db_client():
-    return get_db_client()
+# ---- SETTINGS ----
+def get_settings_dep(settings: AppSettings = Depends(get_settings)):
+    return settings
 
+# ---- OTHER MEMORY OPS ----
 
-# ---- STM Buffer Service ----
-@lru_cache()
-def get_stm_buffer_service() -> STMBufferOps:
-    return STMBufferOps(
-        db_client=get_cached_db_client(),
-        queries=stm_buffer_queries,
+def get_semantic_memory_service(db=Depends(get_db_client_dep)) -> SemanticMemoryOps:
+    return SemanticMemoryOps(
+        db_client=db,
+        queries=semantic_memory_queries,
     )
 
-
-# ---- Session State Service ----
-@lru_cache()
-def get_session_state_service() -> SessionStateOps:
-    return SessionStateOps(
-        db_client=get_cached_db_client(),
-        queries=session_queries,
-    )
-
-
-# ---- Working Memory Service ----
-@lru_cache()
-def get_working_memory_service() -> WorkingMemoryOps:
-    return WorkingMemoryOps(
-        db_client=get_cached_db_client(),
-        queries=working_queries,
-    )
-
-
-# ---- Conversation Service ----
-@lru_cache()
-def get_conversation_service() -> ConversationOps:
-    return ConversationOps(
-        db_client=get_cached_db_client(),
-        queries=conversation_queries,
-    )
-
-
-# ---- Analytics Service ----
-@lru_cache()
-def get_analytics_service() -> AnalyticsOps:
+def get_analytics_service(db=Depends(get_db_client_dep)) -> AnalyticsOps:
     return AnalyticsOps(
-        db_client=get_cached_db_client(),
+        db_client=db,
         queries=analytics_queries,
     )
 
-
-# ---- Transactions Service ----
-@lru_cache()
-def get_transactions_service() -> TransactionsOps:
+def get_transactions_service(db=Depends(get_db_client_dep)) -> TransactionsOps:
     return TransactionsOps(
-        db_client=get_cached_db_client(),
+        db_client=db,
         queries=transactions_queries,
     )
 
-
-# ---- Tags Service ----
-@lru_cache()
-def get_tags_service() -> TagsOps:
-    return TagsOps(
-        db_client=get_cached_db_client(),
-        queries=tags_queries,
-    )
-
-
-# ---- Vector Service ----
-@lru_cache()
-def get_vector_service() -> VectorOps:
+def get_vector_service(db=Depends(get_db_client_dep)) -> VectorOps:
     return VectorOps(
-        db_client=get_cached_db_client(),
+        db_client=db,
         queries=vector_queries,
     )
 
-
-# ---- User Facts Service ----
-@lru_cache()
-def get_user_facts_service() -> UserFactsOps:
+def get_user_facts_service(db=Depends(get_db_client_dep)) -> UserFactsOps:
     return UserFactsOps(
-        db_client=get_cached_db_client(),
+        db_client=db,
         queries=user_facts_queries,
+    )
+
+# ---- MEMORY SYSTEM BUILDERS ----
+    
+def get_memory_pipeline(
+    semantic_memory_operations: SemanticMemoryOps = Depends(get_semantic_memory_service),
+    transactions_operations: TransactionsOps = Depends(get_transactions_service),
+    user_facts_operations: UserFactsOps = Depends(get_user_facts_service),
+    safe_generator=Depends(get_safe_generator),
+    embedder=Depends(get_embedding)
+):
+    return MemoryPipeline(
+        transactions_ops=transactions_operations,
+        semantic_memory_ops=semantic_memory_operations,
+        user_facts_ops=user_facts_operations,
+        safe_generator=safe_generator,
+        embedder=embedder
     )
