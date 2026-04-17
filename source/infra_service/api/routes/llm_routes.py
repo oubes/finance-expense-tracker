9c94c71c6723807859625f6ec977591d325114ee
oubes <omargamal.bs99@gmail.com>
2026-04-17 23:03:50 +0200

    feat: implement generate endpoint for LLM service

--- source/infra_service/api/routes/llm_routes.py
+++ source/infra_service/api/routes/llm_routes.py
@@ -1,5 +1,14 @@
 from fastapi import APIRouter, Response, status
+from source.infra_service.core.di.dependencies import get_llm_service
 
 router = APIRouter()
 
-    
+@router.post("/generate")
+async def generate(message: list[dict[str, str]]):
+    llm_service = await get_llm_service()
+    response = await llm_service.generate(message)
+    return Response(
+        content=response,
+        status_code=status.HTTP_200_OK,
+        media_type="application/json"
+    )
