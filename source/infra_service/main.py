519333a4c7358fbd92899c88ec5cb7ca0bbbb89f
oubes <omargamal.bs99@gmail.com>
2026-04-17 23:03:41 +0200

    feat: add logging setup with console and rotating file handlers

--- source/infra_service/main.py
+++ source/infra_service/main.py
@@ -1,7 +1,9 @@
-import asyncio
-asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
-
-
+# ---- Imports ----
+from source.infra_service.core.observability.logging import setup_logging
 from source.infra_service.core.bootstrap.app_factory import create_app
 
+# ---- Setup Logging ----
+setup_logging()
+
+# ---- Create FastAPI App ----
 app = create_app()
