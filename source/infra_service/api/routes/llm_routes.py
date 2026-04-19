13ab8322297bfdbe2e731e3381c792c9a27c5475
oubes <omargamal.bs99@gmail.com>
2026-04-19 16:31:21 +0200

    feat: update generate endpoint to use ChatMessage model for structured input

--- source/infra_service/api/routes/llm_routes.py
+++ source/infra_service/api/routes/llm_routes.py
@@ -1,18 +1,27 @@
 from fastapi import APIRouter, status, Depends
 from fastapi.responses import JSONResponse
 from source.infra_service.application.services.llm_service import LLMService
+from source.infra_service.api.models.llm_model import ChatMessage
 from source.infra_service.core.di.dependencies import get_llm_service
 
 router = APIRouter()
 
 @router.post("/generate")
 async def generate(
-    message: list[dict[str, str]],
+    message: ChatMessage,
     llm_service: LLMService = Depends(get_llm_service)
 ):
-    response = await llm_service.generate(message)
+
+    messages = [
+        {"role": "system", "content": message.ai_message},
+        {"role": "user", "content": message.user_message},
+    ]
+    response = await llm_service.generate(
+        messages,
+        message.temperature,
+    )
+
     return JSONResponse(
         content=response,
         status_code=status.HTTP_200_OK,
-        media_type="application/json"
     )
